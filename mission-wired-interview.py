64755a65c82c76eab395346d445810a38a244488
brandonwall <blwallerson@gmail.com>
2023-02-21 13:35:45 -0500

    Updated code with source variable from the constituents csv

--- mission-wired-interview.py
+++ mission-wired-interview.py
@@ -7,7 +7,7 @@ from datetime import datetime
 print("Getting Data")
 
 #Get Data and Save as Pandas Dataframe
-constituents = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv',usecols = ['cons_id','create_dt','modified_dt'])
+constituents = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv',usecols = ['cons_id','source','create_dt','modified_dt'])
 emails = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv', usecols = ['email','cons_id','cons_email_id'])
 subscriptions = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv',usecols = ['cons_email_id','isunsub','chapter_id'])
 
@@ -16,17 +16,14 @@ print("Finished Getting Data")
 #Get Data and Save as Pandas
 temp_res = emails.merge(subscriptions, on='cons_email_id')
 final_res =  temp_res.merge(constituents, on='cons_id')
-# final_res.drop('cons_email_id', inplace=True, axis=1)
-# final_res.drop('cons_id', inplace=True, axis=1)
 final_res_filtered=final_res.loc[final_res['chapter_id']==1].copy(deep=True)
 final_res_filtered.pop('chapter_id')
 final_res_filtered.pop('cons_id')
 final_res_filtered.pop('cons_email_id')
-
 final_res_filtered.rename(columns={'isunsub': 'is_unsub'}, inplace=True)
+final_res_filtered.rename(columns={'source': 'code'}, inplace=True)
 
-
-final_res_filtered = final_res_filtered[['email', 'is_unsub', 'create_dt','modified_dt']]
+final_res_filtered = final_res_filtered[['email', 'code', 'is_unsub', 'create_dt','modified_dt']]
 final_res_filtered.to_csv('people.csv', encoding='utf-8', index=False)
 
 print("Creating Acquisition CSV.")
